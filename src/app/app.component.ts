7fa68e7de8645925ac33a10494fbc470f7feaba8
zetter <zetter.e@gmail.com>
2020-05-08 09:54:39 -0500

    Syntaxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'pw-gen';
+export class AppComponent { // AppComponent is a class
+    password = '';  // This line here is the EXACT equivalent of doing the following in vanilla JS: constructor(){this.password='';}
+
+    onButtonClick() {
+        this.password = 'MY PASSWORD!'  // In here, "this" is AppComponent. 
+        // console.log('Button has been clicked');  // <<< Example used earlier to check things were working
+    }
+
+    // The function below would ALSO be requiered if we used [value]="getPassword()" in the html template...
+    // getPassword() {
+    //     return this.password;    
+    // }
+
+    getName() {
+        return 'Isay';
+    }
+
 }
